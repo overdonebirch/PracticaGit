85214463235b4e687891420d68db50a759c57e6f
overdonebirch <105995434+overdonebirch@users.noreply.github.com>
2025-03-31 21:40:42 +0200

    segundo arreglo de bugFix desde gitHub

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const sayHi = () => {
 
     console.log("hola desde github")
-
+    console.log("creada bugFix")
+    console.log("primer arreglo de bugFix")
+    console.log("segundo arreglo de bugFix")
 }
 
 sayHi();
